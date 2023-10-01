bce4ee35e3bfed7e3de605c8e5df36b3aae16b81
Bruno Silva <bruno.silva@xogito.com>
2023-10-01 01:07:59 -0300

    feat(database): creates prisma migrations for task table

--- taskger/prisma/migrations/20230930210430_create_task_table/migration.sql
+++ taskger/prisma/migrations/20230930210430_create_task_table/migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE "task" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3),
     "status" "task_status_type" NOT NULL,
 
